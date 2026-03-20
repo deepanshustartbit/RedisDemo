a41d4e88f8c574f2717d151a8a043d3079e7d74a
Deepanshu Prajapati <deepanshuprajapati@startbitsolutions.com>
2026-03-20 14:04:08 +0530

    Add project files.

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using RedisDemo;
+
+var redis = new RedisService();
+
+// Save data
+await redis.SetAsync("name", "Deepanshu 🔥");
+
+// Get data
+var value = await redis.GetAsync("name");
+
+Console.WriteLine(value);
+
+Console.ReadLine(); // add this
---
+++ RedisDemo/RedisService.cs
@@ -0,0 +1,28 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedisDemo
+{
+    public class RedisService
+    {
+        private readonly IDatabase _db;
+
+        public RedisService()
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            _db = redis.GetDatabase();
+        }
+
+        public async Task SetAsync(string key, string value)
+        {
+            await _db.StringSetAsync(key, value);
+        }
+
+        public async Task<string> GetAsync(string key)
+        {
+            return await _db.StringGetAsync(key);
+        }
+    }
+}
